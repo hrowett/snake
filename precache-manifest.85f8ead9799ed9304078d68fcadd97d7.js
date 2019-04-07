debd036f0ce7bc683d890c7f08ec41e6f4d6d652
hrowett <henry.rowett@gmail.com>
2019-04-07 18:25:25 +0100

    Updates

---
+++ precache-manifest.85f8ead9799ed9304078d68fcadd97d7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "80fd6f16c19a2a907c8e",
+    "url": "/hrowett/hrowett.github.io/snake/static/js/runtime~main.80fd6f16.js"
+  },
+  {
+    "revision": "789767a31e599d12b547",
+    "url": "/hrowett/hrowett.github.io/snake/static/js/main.789767a3.chunk.js"
+  },
+  {
+    "revision": "b3fda22514457bf58940",
+    "url": "/hrowett/hrowett.github.io/snake/static/js/1.b3fda225.chunk.js"
+  },
+  {
+    "revision": "789767a31e599d12b547",
+    "url": "/hrowett/hrowett.github.io/snake/static/css/main.5298909c.chunk.css"
+  },
+  {
+    "revision": "b3fda22514457bf58940",
+    "url": "/hrowett/hrowett.github.io/snake/static/css/1.a03ebbeb.chunk.css"
+  },
+  {
+    "revision": "3b8b8f2f7226fe9c76fd219c0187f494",
+    "url": "/hrowett/hrowett.github.io/snake/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9cb7d6cddbd56d74df96630a2cb27528.js"
+  "/hrowett/hrowett.github.io/snake/precache-manifest.85f8ead9799ed9304078d68fcadd97d7.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/hrowett/hrowett.github.io/snake/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
