ea6c649ec05db80a4500ac3c816bc662ccc154d5
hrowett <henry.rowett@gmail.com>
2019-04-07 17:54:25 +0100

    Updates

---
+++ precache-manifest.737d7558f553eb002b94dc6c43924706.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4fde890f986fd822a049",
+    "url": "/hrowett/snake/static/js/runtime~main.4fde890f.js"
+  },
+  {
+    "revision": "f56a5278018ebc83f01b",
+    "url": "/hrowett/snake/static/js/main.f56a5278.chunk.js"
+  },
+  {
+    "revision": "b3fda22514457bf58940",
+    "url": "/hrowett/snake/static/js/1.b3fda225.chunk.js"
+  },
+  {
+    "revision": "f56a5278018ebc83f01b",
+    "url": "/hrowett/snake/static/css/main.5298909c.chunk.css"
+  },
+  {
+    "revision": "b3fda22514457bf58940",
+    "url": "/hrowett/snake/static/css/1.a03ebbeb.chunk.css"
+  },
+  {
+    "revision": "b3d827356a6973ad8cab10efb546ff45",
+    "url": "/hrowett/snake/index.html"
+  }
+];
