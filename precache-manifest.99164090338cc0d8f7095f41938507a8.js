55d0215e55816c8c18d487a583e2e4920967ea78
hrowett <henry.rowett@gmail.com>
2019-04-08 20:25:08 +0100

    Updates

---
+++ precache-manifest.99164090338cc0d8f7095f41938507a8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "da1ecade61dd97107e27",
+    "url": "/snake/static/js/runtime~main.da1ecade.js"
+  },
+  {
+    "revision": "540a7d22aeded891643a",
+    "url": "/snake/static/js/main.540a7d22.chunk.js"
+  },
+  {
+    "revision": "36a7367ec8464555a5b1",
+    "url": "/snake/static/js/1.36a7367e.chunk.js"
+  },
+  {
+    "revision": "540a7d22aeded891643a",
+    "url": "/snake/static/css/main.b159b669.chunk.css"
+  },
+  {
+    "revision": "36a7367ec8464555a5b1",
+    "url": "/snake/static/css/1.7e141f40.chunk.css"
+  },
+  {
+    "revision": "a9dbabed55737825087dd2ceadb18e69",
+    "url": "/snake/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9cb7d6cddbd56d74df96630a2cb27528.js"
+  "/snake/precache-manifest.99164090338cc0d8f7095f41938507a8.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/snake/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
