a3883bf8b402a99afafd8a1f5c69d7e1166fbf38
hrowett <henry.rowett@gmail.com>
2019-04-17 20:35:28 +0100

    update game board size to 20 squares and control speed

--- src/components/snake/constants/constants.js
+++ src/components/snake/constants/constants.js
@@ -1,10 +1,10 @@
 /**
  * Constants which the game uses to run.
  */
-export const INITIAL_BOARD_SIZE = 18;
+export const INITIAL_BOARD_SIZE = 20;
 export const MAX_BOARD_SIZE = 30;
 export const MIN_BOARD_SIZE = 15;
-export const MIN_GAME_SPEED = 20;
+export const MIN_GAME_SPEED = 0;
 export const INITIAL_GAME_SPEED = 50;
 export const MAX_GAME_SPEED = 100;
 export const SQUARE_SIZE = 20;
